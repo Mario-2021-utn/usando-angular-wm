3663c5aa3b44b48b84c7f69584ed25e28eb6f087
Mario-utn-2021 <mperezquiroga@frba.utn.edu.ar>
2021-07-15 20:18:58 -0300

    Enviando y recibiendo datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gatitos';
+  subtitle = 'Todo lo que necesitas saber'
 }
--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class TitleComponent implements OnInit {
 
   constructor() { }
+
+  // title = "Titulo del h1";
+  // subtitle = "Subtitulo del h1"
+
+  @Input() title?: string;
+  @Input() subtitle?: string;
 
   ngOnInit(): void {
   }
