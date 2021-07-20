3184f36eee0d983d42eafbba6d46a54717e46746
Mario-utn-2021 <mperezquiroga@frba.utn.edu.ar>
2021-07-19 21:28:53 -0300

    pusimos varias card mas como prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'gatitos';
   subtitle = 'Todo lo que necesitas saber'
+
+  cards = [ 
+    {
+      imagen : "/assets/images/autocaro.jpg",
+      categoria: "Alquiler de Autos",
+      titulo: "Australia",
+      precio: 63543
+    },
+    {
+      imagen : "/assets/images/autoblanco.jpg",
+      categoria: "Alquiler de Autos",
+      titulo: "Brasil",
+      precio: 3500
+    },
+    {
+      imagen : "/assets/images/falcon.jpg",
+      categoria: "Alquiler de Autos",
+      titulo: "Argentina",
+      precio: 4600
+    },
+    {
+      imagen : "/assets/images/descarga.jfif",
+      categoria: "Alquiler de Autos",
+      titulo: "Mexico",
+      precio: 16000
+    }
+  ]
 }
