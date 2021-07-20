7a550364cd4531f73d5e0d5d08fc796e7bf71e49
Mario-utn-2021 <mperezquiroga@frba.utn.edu.ar>
2021-07-20 20:51:59 -0300

    click

--- src/app/pages/nosotros/nosotros.component.ts
+++ src/app/pages/nosotros/nosotros.component.ts
@@ -9,8 +9,19 @@ export class NosotrosComponent implements OnInit {
 
   constructor() { }
   
+  saludar(){
+    alert("Oh, hiciste click!");
+  }
+
   title="Seccion Nosotros";
   subtitle="Quienes Somos";
+  mostrar = true;
+
+  toggleInformation() {
+    // invertimos el estado
+
+    this.mostrar = !this.mostrar;
+  }
   
   ngOnInit(): void {
   }
